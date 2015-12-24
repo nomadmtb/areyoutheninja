0c069d68bfa5293a4c27df2df7578c4544855960
Kyle Luce <kylegluce@gmail.com>
2015-12-24 13:43:46 -0800

    Adding lib for Imgur API

--- ninja/lib/imgur.py
+++ ninja/lib/imgur.py
@@ -6,12 +6,33 @@ class ImgurAPI:
       self.__clientid = None
       token_file = None
 
+      # Try to open the file in read-mode
       try:
          token_file = open("TOKENS.txt", "r")
       except:
          pass
 
+      # Try to read the first line with the client-id
       try:
-         self.__clientid = token_file.read_line()
+         self.__clientid = token_file.readline().strip('\n')
       except:
          pass
+
+      # Print the client-id as a sanity check
+      print("ClientID = {0}".format(self.__clientid))
+
+      # Close the file
+      token_file.close()
+
+   # Function will get a random image from the requested channel
+   def get_random_image(self, channel):
+
+      if self.__clientid is None:
+         raise ValueError("ClientID not initialized")
+
+      header = {
+            "Authorization": "Client-ID {0}".format(self.__clientid)
+      }
+
+      json_str = requests.get(channel, headers=header)
+      print(json_str.text)
