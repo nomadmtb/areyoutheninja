7fdbfaa7191b2b84d01e0795c6123e32eec0e363
Kyle Luce <kylegluce@gmail.com>
2015-12-28 14:50:36 -0800

    Basic functionality

--- static/js/ninja.js
+++ static/js/ninja.js
@@ -1,5 +1,14 @@
 // Init variables here
 var reveal_state = 'closed';
+var ninja_data = null;
+
+var data_template = `
+<div id="result">
+<h2>{{ is_ninja }}</h2>
+<p>{{ message }}</p>
+<img src="{{ image }}">
+</div>
+`;
 
 // Function that will toggle the view reveal window
 function toggleReveal() {
@@ -17,8 +26,48 @@ function toggleReveal() {
    }
 }
 
+// Click the test button
+function testNinja() {
+   console.log("Testing for ninja");
+
+   // Turn on the modal
+   $('#myModal').modal('show');
+
+   // Get the data from the api endpoint
+   $.get("/api/isninja", function(data) {
+      ninja_data = data;
+      console.log(ninja_data);
+
+      // Apply the new data to the view
+      applyData(data);
+   });
+}
+
+// Function to apply the data to the view after load
+function applyData(data) {
+   console.log("Applying data to the view");
+
+   var dest = $("#reveal_content");
+   var formatted_data = data_template.replace("{{ is_ninja }}", data["is_ninja"]);
+   formatted_data = formatted_data.replace("{{ message }}", data["message"]);
+   formatted_data = formatted_data.replace("{{ image }}", data["image"]);
+
+   console.log(formatted_data);
+
+   $("#reveal_content").html(formatted_data);
+
+   // Wait for the data to load before showing results
+   var time = setTimeout(function() {
+      // Turn off modal
+      $('#myModal').modal('hide');
+      // Toggle the content
+      toggleReveal();
+   }, 3000);
+}
+
 // Document is ready
 $(function() {
    console.log("Ready");
    $("#reveal_content").toggle();
+   $("#test_btn").click(testNinja);
 });
