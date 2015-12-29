bced765ee25852a3d1c62ff42e558fa6aa4a08c7
Kyle Luce <kylegluce@gmail.com>
2015-12-28 22:08:20 -0800

    Adding DB model etc

--- ninja/models.py
+++ ninja/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+import hashlib
+from datetime import datetime
 
 # Create your models here.
+
+class NinjaResult(models.Model):
+
+   test_date = models.DateTimeField(auto_now_add=True, blank=False, null=False)
+   ip_address = models.CharField(max_length=200, blank=False, null=False)
+   uuid = models.CharField(max_length=250, blank=False, null=False)
+   image = models.CharField(max_length=200, blank=False, null=False)
+   message = models.CharField(max_length=300, blank=False, null=False)
+
+   def save(self, *args, **kwargs):
+
+      # Generate a new uuid if needed
+      if self.pk is None:
+         self.message_id = hashlib.sha1(str(datetime.now()) + self.ip_address + "SaltyMcSaltSalt").hexdigest()
+
+      # Call built-in save method
+      super(NinjaResult, self).save(*args, **kwargs)
