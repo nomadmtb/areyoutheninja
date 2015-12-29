aaad09458f3a374df32f2fad4a83192d523dcee4
Kyle Luce <kylegluce@gmail.com>
2015-12-28 22:11:15 -0800

    adding db back in

--- areyoutheninja/settings.py
+++ areyoutheninja/settings.py
@@ -58,6 +58,10 @@ WSGI_APPLICATION = 'areyoutheninja.wsgi.application'
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
 DATABASES = {
+      'default': {
+         'ENGINE': 'django.db.backends.sqlite3',
+         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+      }
 }
 
 # Internationalization
