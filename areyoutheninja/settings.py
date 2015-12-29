0e89ba15b9e80d4906c2b09c55f91138520210bd
Kyle Luce <kylegluce@gmail.com>
2015-12-29 08:20:38 -0800

    Migrating to MySQL

--- areyoutheninja/settings.py
+++ areyoutheninja/settings.py
@@ -59,8 +59,12 @@ WSGI_APPLICATION = 'areyoutheninja.wsgi.application'
 
 DATABASES = {
       'default': {
-         'ENGINE': 'django.db.backends.sqlite3',
-         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+         'ENGINE': 'django.db.backends.mysql',
+         'NAME': 'ninja',
+         'USER': 'ninja',
+         'PASSWORD': 'password',
+         'HOST': 'localhost',
+         'PORT': '3306',
       }
 }
 
